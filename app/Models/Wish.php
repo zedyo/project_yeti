15f3b48ae3bda8e1962bf16fb07963dd4667b42b
zedyo <ze.d@me.com>
2022-03-27 16:22:08 +0200

    Create essential Wish Backend

---
+++ app/Models/Wish.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Wish extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'employee_id',
+        'shift_id',
+        'day',
+        'month',
+        'year',
+        'creation_date'
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class, 'employee_id');
+    }
+
+    public function shift()
+    {
+        return $this->belongsTo(Shift::class, 'shift_id');
+    }
+}
---
+++ database/migrations/2022_03_27_141440_create_wishes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wishes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('employee_id');
+            $table->unsignedBigInteger('shift_id');
+            $table->unsignedInteger('day');
+            $table->unsignedInteger('month');
+            $table->unsignedInteger('year');
+            $table->timestamps();
+
+            $table->foreign('employee_id')->references('id')->on('employees');
+            $table->foreign('shift_id')->references('id')->on('shifts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wishes');
+    }
+}
