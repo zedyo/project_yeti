d6b0e4986fae68e5054bad70a3b81424f961bf12
zedyo <ze.d@me.com>
2022-05-18 18:05:49 +0200

    Create Preference Model

--- app/Models/Preference.php
+++ app/Models/Preference.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Preference extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'employee_id',
+        'shift_id',
+        'creation_date'
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class, 'employee_id');
+    }
+
+    public function shift()
+    {
+        return $this->belongsTo(Shift::class, 'shift_id');
+    }
 }
