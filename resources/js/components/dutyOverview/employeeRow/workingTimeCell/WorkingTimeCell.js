d45fe058159d82f8349caa1326a863660431161e
zedyo <ze.d@me.com>
2022-05-24 16:51:21 +0200

    Elementar WorkingTimeCell

--- resources/js/components/dutyOverview/employeeRow/workingTimeCell/WorkingTimeCell.js
+++ resources/js/components/dutyOverview/employeeRow/workingTimeCell/WorkingTimeCell.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import './WorkingTimeCell.scss'
 
 function WorkingTime(props) {
-  let value = props.allDuties.map((dutyData) => {
-    // console.log(dutyData);
-  })
+  const { dutiesData } = useSelector((store) => store.duties)
+
+  const employeeDuties = dutiesData.filter(
+    (duty) => duty.employee_id == props.employeeId
+  )
+
+  let workingTime = 0.0
+
+  employeeDuties.map(
+    (duty) => (workingTime = workingTime + parseFloat(duty.shift.h_duration))
+  )
+
+  console.log(workingTime)
+
   // console.log("Test: " + props.allDuties);
-  return <input className="input" value="--.--" disabled={true} />
+  return (
+    <div className="workingTimeCell">
+      <div className="sum">{workingTime.toFixed(2)}</div>
+      <div className="diff">123</div>
+    </div>
+  )
 }
 
 export default WorkingTime
