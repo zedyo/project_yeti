eaccf80be37ceb5c0523fc6bcd8a033351aaee40
zedyo <ze.d@me.com>
2022-05-24 17:31:13 +0200

    Popover on Employees in Row experemental

--- resources/js/components/dutyOverview/employeeRow/employeeCell/EmployeeCell.js
+++ resources/js/components/dutyOverview/employeeRow/employeeCell/EmployeeCell.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import './EmployeeCell.scss'
+import { Popover, OverlayTrigger } from 'react-bootstrap'
 
 function EmployeeCell(props) {
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">
+        {props.employeeData.first_name} {props.employeeData.last_name}
+      </Popover.Header>
+      <Popover.Body>
+        <div>Neuer Wunsch</div>
+        <div>Mitarbeiterdetails</div>
+      </Popover.Body>
+    </Popover>
+  )
+
   return (
     <>
       <div className="employeeContainer">
         <div>
-          <p className="employeeName">
+          {/* <OverlayTrigger trigger="click" placement="right" overlay={popover}> */}
+          <div className="employeeName">
             {props.employeeData.first_name} {props.employeeData.last_name}
-          </p>
-          <p className="employeeQualification">
+          </div>
+          {/* </OverlayTrigger> */}
+          <div className="employeeQualification">
             {props.employeeData.qualification.description}
-          </p>
+          </div>
         </div>
       </div>
     </>
